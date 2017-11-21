91738f94363cd421d49a599eca2a4103cca735c9
lichtRito <b6327@oic.jp>
2017-11-21 15:59:35 +0900

    画面の配置と、５％くらいのコード

--- Order Information Control System/OICSystem/panel/Goods/GoodsRegi.Designer.cs
+++ Order Information Control System/OICSystem/panel/Goods/GoodsRegi.Designer.cs
@@ -31,18 +31,28 @@
             this.goodsRegiId = new System.Windows.Forms.Label();
             this.goodsRegiName = new System.Windows.Forms.Label();
             this.goodsRegiPrice = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.登録 = new System.Windows.Forms.Button();
+            this.IDText = new System.Windows.Forms.TextBox();
+            this.NameText = new System.Windows.Forms.TextBox();
+            this.PriceText = new System.Windows.Forms.TextBox();
+            this.regiBtn = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.picture = new System.Windows.Forms.Panel();
+            this.label10 = new System.Windows.Forms.Label();
+            this.RemText = new System.Windows.Forms.TextBox();
+            this.UmberText = new System.Windows.Forms.TextBox();
+            this.RemLbl = new System.Windows.Forms.Label();
+            this.UmberLbl = new System.Windows.Forms.Label();
+            this.CategoryText = new System.Windows.Forms.ComboBox();
+            this.CategoryLbl = new System.Windows.Forms.Label();
+            this.ImageText = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.picture.SuspendLayout();
             this.SuspendLayout();
             // 
             // goodsRegiId
             // 
             this.goodsRegiId.AutoSize = true;
-            this.goodsRegiId.Location = new System.Drawing.Point(76, 77);
+            this.goodsRegiId.Location = new System.Drawing.Point(76, 81);
             this.goodsRegiId.Name = "goodsRegiId";
             this.goodsRegiId.Size = new System.Drawing.Size(18, 12);
             this.goodsRegiId.TabIndex = 0;
@@ -52,7 +62,7 @@
             // goodsRegiName
             // 
             this.goodsRegiName.AutoSize = true;
-            this.goodsRegiName.Location = new System.Drawing.Point(63, 132);
+            this.goodsRegiName.Location = new System.Drawing.Point(63, 130);
             this.goodsRegiName.Name = "goodsRegiName";
             this.goodsRegiName.Size = new System.Drawing.Size(31, 12);
             this.goodsRegiName.TabIndex = 1;
@@ -62,42 +72,43 @@
             // goodsRegiPrice
             // 
             this.goodsRegiPrice.AutoSize = true;
-            this.goodsRegiPrice.Location = new System.Drawing.Point(63, 187);
+            this.goodsRegiPrice.Location = new System.Drawing.Point(63, 179);
             this.goodsRegiPrice.Name = "goodsRegiPrice";
             this.goodsRegiPrice.Size = new System.Drawing.Size(31, 12);
             this.goodsRegiPrice.TabIndex = 2;
             this.goodsRegiPrice.Text = "単価:";
             this.goodsRegiPrice.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
-            // textBox1
+            // IDText
             // 
-            this.textBox1.Location = new System.Drawing.Point(100, 74);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(264, 19);
-            this.textBox1.TabIndex = 3;
+            this.IDText.Location = new System.Drawing.Point(100, 74);
+            this.IDText.Name = "IDText";
+            this.IDText.Size = new System.Drawing.Size(228, 19);
+            this.IDText.TabIndex = 3;
             // 
-            // textBox2
+            // NameText
             // 
-            this.textBox2.Location = new System.Drawing.Point(100, 129);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(264, 19);
-            this.textBox2.TabIndex = 4;
+            this.NameText.Location = new System.Drawing.Point(100, 123);
+            this.NameText.Name = "NameText";
+            this.NameText.Size = new System.Drawing.Size(228, 19);
+            this.NameText.TabIndex = 4;
             // 
-            // textBox3
+            // PriceText
             // 
-            this.textBox3.Location = new System.Drawing.Point(100, 184);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(264, 19);
-            this.textBox3.TabIndex = 5;
+            this.PriceText.Location = new System.Drawing.Point(100, 172);
+            this.PriceText.Name = "PriceText";
+            this.PriceText.Size = new System.Drawing.Size(228, 19);
+            this.PriceText.TabIndex = 5;
             // 
-            // 登録
+            // regiBtn
             // 
-            this.登録.Location = new System.Drawing.Point(593, 353);
-            this.登録.Name = "登録";
-            this.登録.Size = new System.Drawing.Size(138, 47);
-            this.登録.TabIndex = 6;
-            this.登録.Text = "登録";
-            this.登録.UseVisualStyleBackColor = true;
+            this.regiBtn.Location = new System.Drawing.Point(593, 353);
+            this.regiBtn.Name = "regiBtn";
+            this.regiBtn.Size = new System.Drawing.Size(138, 47);
+            this.regiBtn.TabIndex = 6;
+            this.regiBtn.Text = "登録";
+            this.regiBtn.UseVisualStyleBackColor = true;
+            this.regiBtn.Click += new System.EventHandler(this.regiBtn_Click);
             // 
             // pictureBox1
             // 
@@ -107,21 +118,119 @@
             this.pictureBox1.TabIndex = 7;
             this.pictureBox1.TabStop = false;
             // 
+            // picture
+            // 
+            this.picture.AllowDrop = true;
+            this.picture.BackColor = System.Drawing.Color.White;
+            this.picture.Controls.Add(this.label10);
+            this.picture.Location = new System.Drawing.Point(466, 38);
+            this.picture.Name = "picture";
+            this.picture.Size = new System.Drawing.Size(265, 265);
+            this.picture.TabIndex = 24;
+            this.picture.DragDrop += new System.Windows.Forms.DragEventHandler(this.picture_DragDrop);
+            this.picture.DragEnter += new System.Windows.Forms.DragEventHandler(this.picture_DragEnter);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(95, 122);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(64, 12);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "ここにドロップ";
+            // 
+            // RemText
+            // 
+            this.RemText.Location = new System.Drawing.Point(100, 271);
+            this.RemText.Name = "RemText";
+            this.RemText.Size = new System.Drawing.Size(228, 19);
+            this.RemText.TabIndex = 25;
+            // 
+            // UmberText
+            // 
+            this.UmberText.Location = new System.Drawing.Point(100, 320);
+            this.UmberText.Name = "UmberText";
+            this.UmberText.Size = new System.Drawing.Size(67, 19);
+            this.UmberText.TabIndex = 26;
+            // 
+            // RemLbl
+            // 
+            this.RemLbl.AutoSize = true;
+            this.RemLbl.Location = new System.Drawing.Point(63, 278);
+            this.RemLbl.Name = "RemLbl";
+            this.RemLbl.Size = new System.Drawing.Size(31, 12);
+            this.RemLbl.TabIndex = 27;
+            this.RemLbl.Text = "備考:";
+            // 
+            // UmberLbl
+            // 
+            this.UmberLbl.AutoSize = true;
+            this.UmberLbl.Location = new System.Drawing.Point(27, 327);
+            this.UmberLbl.Name = "UmberLbl";
+            this.UmberLbl.Size = new System.Drawing.Size(67, 12);
+            this.UmberLbl.TabIndex = 28;
+            this.UmberLbl.Text = "定期発注数:";
+            // 
+            // CategoryText
+            // 
+            this.CategoryText.AutoCompleteCustomSource.AddRange(new string[] {
+            "GK",
+            "DF",
+            "MF",
+            "FW"});
+            this.CategoryText.FormattingEnabled = true;
+            this.CategoryText.Items.AddRange(new object[] {
+            "GK",
+            "DF",
+            "FW",
+            "MF"});
+            this.CategoryText.Location = new System.Drawing.Point(100, 221);
+            this.CategoryText.Name = "CategoryText";
+            this.CategoryText.Size = new System.Drawing.Size(121, 20);
+            this.CategoryText.TabIndex = 29;
+            // 
+            // CategoryLbl
+            // 
+            this.CategoryLbl.AutoSize = true;
+            this.CategoryLbl.Location = new System.Drawing.Point(53, 229);
+            this.CategoryLbl.Name = "CategoryLbl";
+            this.CategoryLbl.Size = new System.Drawing.Size(41, 12);
+            this.CategoryLbl.TabIndex = 30;
+            this.CategoryLbl.Text = "カテゴリ:";
+            // 
+            // ImageText
+            // 
+            this.ImageText.Location = new System.Drawing.Point(563, 13);
+            this.ImageText.Name = "ImageText";
+            this.ImageText.ReadOnly = true;
+            this.ImageText.Size = new System.Drawing.Size(100, 19);
+            this.ImageText.TabIndex = 31;
+            // 
             // GoodsRegi
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.ImageText);
+            this.Controls.Add(this.CategoryLbl);
+            this.Controls.Add(this.CategoryText);
+            this.Controls.Add(this.UmberLbl);
+            this.Controls.Add(this.RemLbl);
+            this.Controls.Add(this.UmberText);
+            this.Controls.Add(this.RemText);
+            this.Controls.Add(this.picture);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.登録);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.regiBtn);
+            this.Controls.Add(this.PriceText);
+            this.Controls.Add(this.NameText);
+            this.Controls.Add(this.IDText);
             this.Controls.Add(this.goodsRegiPrice);
             this.Controls.Add(this.goodsRegiName);
             this.Controls.Add(this.goodsRegiId);
             this.Name = "GoodsRegi";
             this.Size = new System.Drawing.Size(760, 430);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.picture.ResumeLayout(false);
+            this.picture.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -132,10 +241,19 @@
         private System.Windows.Forms.Label goodsRegiId;
         private System.Windows.Forms.Label goodsRegiName;
         private System.Windows.Forms.Label goodsRegiPrice;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Button 登録;
+        private System.Windows.Forms.TextBox IDText;
+        private System.Windows.Forms.TextBox NameText;
+        private System.Windows.Forms.TextBox PriceText;
+        private System.Windows.Forms.Button regiBtn;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel picture;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox RemText;
+        private System.Windows.Forms.TextBox UmberText;
+        private System.Windows.Forms.Label RemLbl;
+        private System.Windows.Forms.Label UmberLbl;
+        private System.Windows.Forms.ComboBox CategoryText;
+        private System.Windows.Forms.Label CategoryLbl;
+        private System.Windows.Forms.TextBox ImageText;
     }
 }
--- Order Information Control System/OICSystem/panel/Goods/GoodsRegi.cs
+++ Order Information Control System/OICSystem/panel/Goods/GoodsRegi.cs
@@ -7,14 +7,67 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Data.OleDb;
 
 namespace WindowsFormsApplication1.panel
 {
     public partial class GoodsRegi : UserControl
     {
+        OleDbConnection cn = new OleDbConnection();
+        OleDbCommand cmd = new OleDbCommand();
+        OleDbDataAdapter da = new OleDbDataAdapter();
+        DataTable dt = new DataTable();
+
         public GoodsRegi()
         {
             InitializeComponent();
+        }
+
+        private void regiBtn_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("ID=" + RemText.Text + "のデータを追加してもよろしいですか", "CS-27", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+            {
+                return;
+            }
+
+            if (ImageText.Text == "")
+            {
+                MessageBox.Show("新規画像がありません。登録済みの画像が設定されます。", "CS-27", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                ImageText.Text = textBox6.Text;
+            }
+
+            cmd.Connection = cn;
+            cmd.CommandText = "INSERT INTO Goods (商品ID, 商品名, 単価, カテゴリID,備考, 定期発注数, 画像ファイル)" +
+                " VALUES (@id,@name,@number,@teamid,@borndate,@height,@weight,@imagefile)";
+            OleDbParameter gdid = new OleDbParameter("@gdid", IDText.Text);
+            cmd.Parameters.Add(gdid);
+            OleDbParameter gdname = new OleDbParameter("@gdname", NameText.Text);
+            cmd.Parameters.Add(gdname);
+            OleDbParameter gdCategoryid = new OleDbParameter("@gdCategoryid", CategoryText.Text);
+            cmd.Parameters.Add(gdCategoryid);
+            OleDbParameter grrem = new OleDbParameter("@teamid", RemText.Text);
+            cmd.Parameters.Add(grrem);
+            OleDbParameter prborndate = new OleDbParameter("@borndate", textBox3.Text);
+            cmd.Parameters.Add(prborndate);
+            OleDbParameter prheight = new OleDbParameter("@height", int.Parse(textBox4.Text));
+            cmd.Parameters.Add(prheight);
+            OleDbParameter prweight = new OleDbParameter("@weight", int.Parse(textBox5.Text));
+            cmd.Parameters.Add(prweight);
+            OleDbParameter primagefile = new OleDbParameter("@imagefile ", ImageText.Text);
+            cmd.Parameters.Add(primagefile);
+        }
+
+        private void picture_DragDrop(object sender, DragEventArgs e)
+        {
+            string dropfile = ((string[])e.Data.GetData(DataFormats.FileDrop))[0];
+            string imagefile = Path.GetFileName(dropfile);
+            string path = System.AppDomain.CurrentDomain.SetupInformation.ApplicationBase + @".\IM2image\" + imagefile;
+        }
+
+        private void picture_DragEnter(object sender, DragEventArgs e)
+        {
+
         }
     }
 }
