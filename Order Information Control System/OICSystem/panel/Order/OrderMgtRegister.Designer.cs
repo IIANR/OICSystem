56cfa97a89fbee03df0211cbbfded5d8c6d380b2
higa3545 <33534879+higa3545@users.noreply.github.com>
2017-11-25 16:11:40 +0900

    グリットビューに商品名、単価を表示

--- Order Information Control System/OICSystem/panel/Order/OrderMgtRegister.Designer.cs
+++ Order Information Control System/OICSystem/panel/Order/OrderMgtRegister.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             this.TelTextbox = new System.Windows.Forms.TextBox();
             this.AddressTextbox1 = new System.Windows.Forms.TextBox();
             this.NameTextbox = new System.Windows.Forms.TextBox();
@@ -55,52 +54,42 @@
             this.OrderRegiDataGridview = new System.Windows.Forms.DataGridView();
             this.NumCombo = new System.Windows.Forms.ComboBox();
             this.ErrMsg = new System.Windows.Forms.Label();
-            this.iM2DataSet = new WindowsFormsApplication1.IM2DataSet();
-            this.商品マスタBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.商品マスタTableAdapter = new WindowsFormsApplication1.IM2DataSetTableAdapters.商品マスタTableAdapter();
             ((System.ComponentModel.ISupportInitialize)(this.OrderRegiDataGridview)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.iM2DataSet)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.商品マスタBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // TelTextbox
             // 
-            this.TelTextbox.Location = new System.Drawing.Point(325, 724);
-            this.TelTextbox.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.TelTextbox.Location = new System.Drawing.Point(150, 362);
             this.TelTextbox.Name = "TelTextbox";
-            this.TelTextbox.Size = new System.Drawing.Size(249, 31);
+            this.TelTextbox.Size = new System.Drawing.Size(117, 19);
             this.TelTextbox.TabIndex = 48;
             // 
             // AddressTextbox1
             // 
-            this.AddressTextbox1.Location = new System.Drawing.Point(325, 612);
-            this.AddressTextbox1.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.AddressTextbox1.Location = new System.Drawing.Point(150, 306);
             this.AddressTextbox1.Name = "AddressTextbox1";
-            this.AddressTextbox1.Size = new System.Drawing.Size(249, 31);
+            this.AddressTextbox1.Size = new System.Drawing.Size(117, 19);
             this.AddressTextbox1.TabIndex = 47;
             // 
             // NameTextbox
             // 
-            this.NameTextbox.Location = new System.Drawing.Point(325, 444);
-            this.NameTextbox.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.NameTextbox.Location = new System.Drawing.Point(150, 222);
             this.NameTextbox.Name = "NameTextbox";
-            this.NameTextbox.Size = new System.Drawing.Size(249, 31);
+            this.NameTextbox.Size = new System.Drawing.Size(117, 19);
             this.NameTextbox.TabIndex = 46;
             // 
             // GoodsidTextBox
             // 
-            this.GoodsidTextBox.Location = new System.Drawing.Point(299, 172);
-            this.GoodsidTextBox.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.GoodsidTextBox.Location = new System.Drawing.Point(138, 86);
             this.GoodsidTextBox.Name = "GoodsidTextBox";
-            this.GoodsidTextBox.Size = new System.Drawing.Size(394, 31);
+            this.GoodsidTextBox.Size = new System.Drawing.Size(184, 19);
             this.GoodsidTextBox.TabIndex = 44;
             // 
             // OrderCompBtn
             // 
-            this.OrderCompBtn.Location = new System.Drawing.Point(1259, 682);
-            this.OrderCompBtn.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.OrderCompBtn.Location = new System.Drawing.Point(581, 341);
             this.OrderCompBtn.Name = "OrderCompBtn";
-            this.OrderCompBtn.Size = new System.Drawing.Size(327, 80);
+            this.OrderCompBtn.Size = new System.Drawing.Size(151, 40);
             this.OrderCompBtn.TabIndex = 43;
             this.OrderCompBtn.Text = "注文完了";
             this.OrderCompBtn.UseVisualStyleBackColor = true;
@@ -108,97 +97,87 @@
             // TelLabel
             // 
             this.TelLabel.AutoSize = true;
-            this.TelLabel.Location = new System.Drawing.Point(163, 730);
-            this.TelLabel.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.TelLabel.Location = new System.Drawing.Point(75, 365);
             this.TelLabel.Name = "TelLabel";
-            this.TelLabel.Size = new System.Drawing.Size(118, 24);
+            this.TelLabel.Size = new System.Drawing.Size(59, 12);
             this.TelLabel.TabIndex = 41;
             this.TelLabel.Text = "電話番号：";
             // 
             // AddressLabel1
             // 
             this.AddressLabel1.AutoSize = true;
-            this.AddressLabel1.Location = new System.Drawing.Point(199, 618);
-            this.AddressLabel1.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.AddressLabel1.Location = new System.Drawing.Point(92, 309);
             this.AddressLabel1.Name = "AddressLabel1";
-            this.AddressLabel1.Size = new System.Drawing.Size(82, 24);
+            this.AddressLabel1.Size = new System.Drawing.Size(41, 12);
             this.AddressLabel1.TabIndex = 40;
             this.AddressLabel1.Text = "住所1：";
             // 
             // NameLabel
             // 
             this.NameLabel.AutoSize = true;
-            this.NameLabel.Location = new System.Drawing.Point(212, 450);
-            this.NameLabel.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.NameLabel.Location = new System.Drawing.Point(98, 225);
             this.NameLabel.Name = "NameLabel";
-            this.NameLabel.Size = new System.Drawing.Size(70, 24);
+            this.NameLabel.Size = new System.Drawing.Size(35, 12);
             this.NameLabel.TabIndex = 39;
             this.NameLabel.Text = "名前：";
             // 
             // MemberLabel
             // 
             this.MemberLabel.AutoSize = true;
-            this.MemberLabel.Location = new System.Drawing.Point(98, 386);
-            this.MemberLabel.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.MemberLabel.Location = new System.Drawing.Point(45, 193);
             this.MemberLabel.Name = "MemberLabel";
-            this.MemberLabel.Size = new System.Drawing.Size(126, 24);
+            this.MemberLabel.Size = new System.Drawing.Size(63, 12);
             this.MemberLabel.TabIndex = 38;
             this.MemberLabel.Text = "お客様情報";
             // 
             // NumLabel
             // 
             this.NumLabel.AutoSize = true;
-            this.NumLabel.Location = new System.Drawing.Point(210, 256);
-            this.NumLabel.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.NumLabel.Location = new System.Drawing.Point(97, 128);
             this.NumLabel.Name = "NumLabel";
-            this.NumLabel.Size = new System.Drawing.Size(70, 24);
+            this.NumLabel.Size = new System.Drawing.Size(35, 12);
             this.NumLabel.TabIndex = 37;
             this.NumLabel.Text = "個数：";
             // 
             // GoodsidLabel
             // 
             this.GoodsidLabel.AutoSize = true;
-            this.GoodsidLabel.Location = new System.Drawing.Point(186, 175);
-            this.GoodsidLabel.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.GoodsidLabel.Location = new System.Drawing.Point(86, 88);
             this.GoodsidLabel.Name = "GoodsidLabel";
-            this.GoodsidLabel.Size = new System.Drawing.Size(92, 24);
+            this.GoodsidLabel.Size = new System.Drawing.Size(46, 12);
             this.GoodsidLabel.TabIndex = 36;
             this.GoodsidLabel.Text = "商品ID：";
             // 
             // OrderGoodsLabel
             // 
             this.OrderGoodsLabel.AutoSize = true;
-            this.OrderGoodsLabel.Location = new System.Drawing.Point(89, 106);
-            this.OrderGoodsLabel.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.OrderGoodsLabel.Location = new System.Drawing.Point(41, 53);
             this.OrderGoodsLabel.Name = "OrderGoodsLabel";
-            this.OrderGoodsLabel.Size = new System.Drawing.Size(106, 24);
+            this.OrderGoodsLabel.Size = new System.Drawing.Size(53, 12);
             this.OrderGoodsLabel.TabIndex = 35;
             this.OrderGoodsLabel.Text = "注文商品";
             // 
             // PoscodeLabel
             // 
             this.PoscodeLabel.AutoSize = true;
-            this.PoscodeLabel.Location = new System.Drawing.Point(163, 562);
-            this.PoscodeLabel.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.PoscodeLabel.Location = new System.Drawing.Point(75, 281);
             this.PoscodeLabel.Name = "PoscodeLabel";
-            this.PoscodeLabel.Size = new System.Drawing.Size(118, 24);
+            this.PoscodeLabel.Size = new System.Drawing.Size(59, 12);
             this.PoscodeLabel.TabIndex = 49;
             this.PoscodeLabel.Text = "郵便番号：";
             // 
             // PoscodeTextbox
             // 
-            this.PoscodeTextbox.Location = new System.Drawing.Point(325, 556);
-            this.PoscodeTextbox.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.PoscodeTextbox.Location = new System.Drawing.Point(150, 278);
             this.PoscodeTextbox.Name = "PoscodeTextbox";
-            this.PoscodeTextbox.Size = new System.Drawing.Size(249, 31);
+            this.PoscodeTextbox.Size = new System.Drawing.Size(117, 19);
             this.PoscodeTextbox.TabIndex = 50;
             // 
             // AddBtn
             // 
-            this.AddBtn.Location = new System.Drawing.Point(845, 682);
-            this.AddBtn.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.AddBtn.Location = new System.Drawing.Point(390, 341);
             this.AddBtn.Name = "AddBtn";
-            this.AddBtn.Size = new System.Drawing.Size(327, 80);
+            this.AddBtn.Size = new System.Drawing.Size(151, 40);
             this.AddBtn.TabIndex = 52;
             this.AddBtn.Text = "追加";
             this.AddBtn.UseVisualStyleBackColor = true;
@@ -207,86 +186,77 @@
             // TotalTitleLabel
             // 
             this.TotalTitleLabel.AutoSize = true;
-            this.TotalTitleLabel.Location = new System.Drawing.Point(880, 620);
-            this.TotalTitleLabel.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.TotalTitleLabel.Location = new System.Drawing.Point(406, 310);
             this.TotalTitleLabel.Name = "TotalTitleLabel";
-            this.TotalTitleLabel.Size = new System.Drawing.Size(118, 24);
+            this.TotalTitleLabel.Size = new System.Drawing.Size(59, 12);
             this.TotalTitleLabel.TabIndex = 54;
             this.TotalTitleLabel.Text = "合計金額：";
             // 
             // TotalLabel
             // 
             this.TotalLabel.AutoSize = true;
-            this.TotalLabel.Location = new System.Drawing.Point(1053, 620);
-            this.TotalLabel.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.TotalLabel.Location = new System.Drawing.Point(486, 310);
             this.TotalLabel.Name = "TotalLabel";
-            this.TotalLabel.Size = new System.Drawing.Size(22, 24);
+            this.TotalLabel.Size = new System.Drawing.Size(11, 12);
             this.TotalLabel.TabIndex = 55;
             this.TotalLabel.Text = "0";
             // 
             // AddressTextbox2
             // 
-            this.AddressTextbox2.Location = new System.Drawing.Point(325, 668);
-            this.AddressTextbox2.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.AddressTextbox2.Location = new System.Drawing.Point(150, 334);
             this.AddressTextbox2.Name = "AddressTextbox2";
-            this.AddressTextbox2.Size = new System.Drawing.Size(249, 31);
+            this.AddressTextbox2.Size = new System.Drawing.Size(117, 19);
             this.AddressTextbox2.TabIndex = 57;
             // 
             // AddressLabel2
             // 
             this.AddressLabel2.AutoSize = true;
-            this.AddressLabel2.Location = new System.Drawing.Point(199, 674);
-            this.AddressLabel2.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.AddressLabel2.Location = new System.Drawing.Point(92, 337);
             this.AddressLabel2.Name = "AddressLabel2";
-            this.AddressLabel2.Size = new System.Drawing.Size(82, 24);
+            this.AddressLabel2.Size = new System.Drawing.Size(41, 12);
             this.AddressLabel2.TabIndex = 56;
             this.AddressLabel2.Text = "住所2：";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(33, 32);
-            this.label1.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.label1.Location = new System.Drawing.Point(15, 16);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(92, 24);
+            this.label1.Size = new System.Drawing.Size(46, 12);
             this.label1.TabIndex = 58;
             this.label1.Text = "注文ID：";
             // 
             // label2
             // 
-            this.label2.Location = new System.Drawing.Point(160, 32);
-            this.label2.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.label2.Location = new System.Drawing.Point(74, 16);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(405, 46);
+            this.label2.Size = new System.Drawing.Size(187, 23);
             this.label2.TabIndex = 59;
             this.label2.Text = "0000";
             // 
             // KanaLabel
             // 
             this.KanaLabel.AutoSize = true;
-            this.KanaLabel.Location = new System.Drawing.Point(199, 506);
-            this.KanaLabel.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.KanaLabel.Location = new System.Drawing.Point(92, 253);
             this.KanaLabel.Name = "KanaLabel";
-            this.KanaLabel.Size = new System.Drawing.Size(82, 24);
+            this.KanaLabel.Size = new System.Drawing.Size(42, 12);
             this.KanaLabel.TabIndex = 60;
             this.KanaLabel.Text = "ﾌﾘｶﾞﾅ：";
             // 
             // KanaTextbox
             // 
-            this.KanaTextbox.Location = new System.Drawing.Point(325, 500);
-            this.KanaTextbox.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.KanaTextbox.Location = new System.Drawing.Point(150, 250);
             this.KanaTextbox.Name = "KanaTextbox";
-            this.KanaTextbox.Size = new System.Drawing.Size(249, 31);
+            this.KanaTextbox.Size = new System.Drawing.Size(117, 19);
             this.KanaTextbox.TabIndex = 61;
             // 
             // OrderRegiDataGridview
             // 
             this.OrderRegiDataGridview.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.OrderRegiDataGridview.Location = new System.Drawing.Point(845, 106);
-            this.OrderRegiDataGridview.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.OrderRegiDataGridview.Location = new System.Drawing.Point(390, 53);
             this.OrderRegiDataGridview.Name = "OrderRegiDataGridview";
             this.OrderRegiDataGridview.RowTemplate.Height = 21;
-            this.OrderRegiDataGridview.Size = new System.Drawing.Size(741, 444);
+            this.OrderRegiDataGridview.Size = new System.Drawing.Size(342, 222);
             this.OrderRegiDataGridview.TabIndex = 62;
             // 
             // NumCombo
@@ -299,37 +269,25 @@
             "4",
             "5",
             "6"});
-            this.NumCombo.Location = new System.Drawing.Point(299, 248);
+            this.NumCombo.Location = new System.Drawing.Point(138, 124);
+            this.NumCombo.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
             this.NumCombo.Name = "NumCombo";
-            this.NumCombo.Size = new System.Drawing.Size(67, 32);
+            this.NumCombo.Size = new System.Drawing.Size(33, 20);
             this.NumCombo.TabIndex = 63;
             this.NumCombo.Text = "1";
             // 
             // ErrMsg
             // 
-            this.ErrMsg.Location = new System.Drawing.Point(299, 209);
+            this.ErrMsg.Location = new System.Drawing.Point(138, 104);
+            this.ErrMsg.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.ErrMsg.Name = "ErrMsg";
-            this.ErrMsg.Size = new System.Drawing.Size(394, 36);
+            this.ErrMsg.Size = new System.Drawing.Size(182, 18);
             this.ErrMsg.TabIndex = 64;
             this.ErrMsg.Text = "エラーメッセージ";
             // 
-            // iM2DataSet
-            // 
-            this.iM2DataSet.DataSetName = "IM2DataSet";
-            this.iM2DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // 商品マスタBindingSource
-            // 
-            this.商品マスタBindingSource.DataMember = "商品マスタ";
-            this.商品マスタBindingSource.DataSource = this.iM2DataSet;
-            // 
-            // 商品マスタTableAdapter
-            // 
-            this.商品マスタTableAdapter.ClearBeforeFill = true;
-            // 
             // OrderMgtRegister
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.ErrMsg);
             this.Controls.Add(this.NumCombo);
@@ -357,13 +315,10 @@
             this.Controls.Add(this.NumLabel);
             this.Controls.Add(this.GoodsidLabel);
             this.Controls.Add(this.OrderGoodsLabel);
-            this.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
             this.Name = "OrderMgtRegister";
-            this.Size = new System.Drawing.Size(1647, 860);
+            this.Size = new System.Drawing.Size(760, 430);
             this.Load += new System.EventHandler(this.OrderMgtRegister_Load);
             ((System.ComponentModel.ISupportInitialize)(this.OrderRegiDataGridview)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.iM2DataSet)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.商品マスタBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -397,8 +352,5 @@
         private System.Windows.Forms.DataGridView OrderRegiDataGridview;
         private System.Windows.Forms.ComboBox NumCombo;
         private System.Windows.Forms.Label ErrMsg;
-        private IM2DataSet iM2DataSet;
-        private System.Windows.Forms.BindingSource 商品マスタBindingSource;
-        private IM2DataSetTableAdapters.商品マスタTableAdapter 商品マスタTableAdapter;
     }
 }
--- Order Information Control System/OICSystem/panel/Order/OrderMgtRegister.cs
+++ Order Information Control System/OICSystem/panel/Order/OrderMgtRegister.cs
@@ -15,95 +15,90 @@ namespace WindowsFormsApplication1
     public partial class OrderMgtRegister : UserControl
     {
         OleDbConnection cn = new OleDbConnection();
+        OleDbCommand cmd = new OleDbCommand();
+        DataTable dt = new DataTable();
 
-        static int sum = 0;
+        int InOrderid = 0;
+        int db_Orderid = 0;
+        string db_Goodsname = "";
+        int db_GoodsPrice = 0;
 
-        private double Tax = 0;
-        DataSet sdt = new DataSet();
 
         public OrderMgtRegister()
         {
             InitializeComponent();
         }
 
+        private DataTable CreateSchemaDataTable(OleDbDataReader reader)
+        {
+            if (reader == null) { return null; }
+            if (reader.IsClosed) { return null; }
+
+            DataTable schema = reader.GetSchemaTable();
+            DataTable dt = new DataTable();
+
+            foreach (DataRow row in schema.Rows)
+            {
+                // Column情報を追加してください。
+                DataColumn col = new DataColumn();
+                col.ColumnName = row["ColumnName"].ToString();
+                col.DataType = Type.GetType(row["DataType"].ToString());
+
+                if (col.DataType.Equals(typeof(string)))
+                {
+                    col.MaxLength = (int)row["ColumnSize"];
+                }
+
+                dt.Columns.Add(col);
+            }
+            return dt;
+        }
+
         public void OrderMgtRegister_Load(object sender, EventArgs e)
         {
-            cn.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;" + @"Data Source=.\DB\IM2.accdb;";
-            OleDbDataAdapter datax = new OleDbDataAdapter("SELECT 税率 FROM Tax", cn);
-            DataTable dttax = new DataTable();
-            datax.Fill(dttax);
-            Tax = double.Parse(dttax.Rows[0][0].ToString());//データテーブルの0行0列のデータ
-            dttax.Clear();
-
-            DataTable dt = new DataTable();
-            dt.Columns.Add(new DataColumn("商品名", Type.GetType("System.String")));
-            dt.Columns.Add(new DataColumn("単価", Type.GetType("System.String")));
-
-            sdt.Tables.Add(dt);          
+            
         }
 
         private void AddBtn_Click(object sender, EventArgs e)
         {
+            //データテーブルクリア
+            dt.Clear();
+            OrderRegiDataGridview.Columns.Clear();
+            OrderRegiDataGridview.DataSource = null;
+
             cn.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;" + @"Data Source=.\DB\IM2.accdb;";
-            OleDbDataAdapter dagoods = new OleDbDataAdapter("SELECT 商品名,単価 FROM 商品マスタ WHERE 商品ID='" + int.Parse(GoodsidTextBox.Text) + "'", cn);
-            DataTable dtgoods = new DataTable();
-            dagoods.Fill(dtgoods);
+            cn.Open();
 
-            ErrMsg.Text = "";
+            cmd.CommandText = "SELECT * FROM 商品マスタ";
+            cmd.Connection = cn;
 
-            if (dtgoods.Rows.Count < 1)
+            OleDbDataReader rd = cmd.ExecuteReader();
+            OrderRegiDataGridview.Columns.Add("GoodsName", "商品名");
+            OrderRegiDataGridview.Columns.Add("GoodsPrice", "単価");
+
+            dt = CreateSchemaDataTable(rd);
+            DataRow row = dt.NewRow();
+
+            InOrderid = int.Parse(GoodsidTextBox.Text);
+
+            int cnt = 0;
+
+            while (rd.Read())
             {
-                ErrMsg.Text = "登録されていない商品です";
-                return;
+                db_Orderid = (int)rd.GetValue(0);
+                db_Goodsname = (string)rd.GetValue(1);
+                db_GoodsPrice = (int)rd.GetValue(2);
+                if (db_Orderid == InOrderid)
+                {
+                    OrderRegiDataGridview.Rows.Add();
+                    OrderRegiDataGridview["GoodsName",cnt].Value = db_Goodsname;
+                    OrderRegiDataGridview["GoodsPrice", cnt].Value = db_GoodsPrice;
+                }
+
             }
 
-
-            //動的なデータテーブルを戻す
-            DataTable dt = sdt.Tables[0];//DataTable型にキャスト
-
-            //反映されるまで待機
-            while (dt == null)
-            {
-                System.Threading.Thread.Sleep(0);
-            }
-
-            //データテーブルに追加する行を作成
-            DataRow dr;
-            dr = dt.NewRow();
-            dr["商品名"] = dtgoods.Rows[0][0].ToString();
-            dr["価格"] = string.Format("{0:#,###}円", int.Parse(dtgoods.Rows[0][1].ToString()));
-
-            //小計を計算
-            sum += int.Parse(dtgoods.Rows[0][1].ToString());
-
-            // データテーブル商品をクリア
-            dtgoods.Clear();
-
-            // データテーブルに行を追加
-            dt.Rows.Add(dr);
-
-            // GridView1にデータをバインド
-            OrderRegiDataGridview.DataSource = dt;
-            //OrderRegiDataGridview.DataBind();
-
-
-            //// 次の入力のために、テキスト入力エリアのクリアとフォーカスの移動
-            //TextBGoodsID.Text = "";
-            //TextBGoodsID.Focus();          //フォーカス
-
-            //// ラベルの表示
-            //LblSumTitle.Visible = true;
-            //LblTaxTitle.Visible = true;
-            //LblGokeiTitle.Visible = true;
-            //LblSum.Visible = true;
-            //LblTax.Visible = true;
-            //LblGokei.Visible = true;
-
-            // 消費税の計算と金額の表示
-            double tax = Tax;
-            //LblSum.Text = string.Format("{0:#,###}円", sum);
-            //LblTax.Text = string.Format("{0:#,###}円", sum * tax);         //消費税
-            TotalLabel.Text = string.Format("{0:#,###}円", sum + sum * tax);  //消費税を加えた合計金額
+            cn.Close();
+            OrderRegiDataGridview.AutoResizeColumns();
         }
     }
 }
