c221118f897218bee72b5840998926b52738b1a1
りむる <31757798+rimurukun@users.noreply.github.com>
2017-12-11 01:02:03 +0900

    商品管理の商品在庫ボタンの完成
    検索する機能を追加
    更新する機能を追加

--- Order Information Control System/OICSystem/panel/Goods/GoodsStock.Designer.cs
+++ Order Information Control System/OICSystem/panel/Goods/GoodsStock.Designer.cs
@@ -28,36 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.GoodsDataGridView = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.GoodsDataGridView)).BeginInit();
+            this.GoodsStockDataGridView = new System.Windows.Forms.DataGridView();
+            this.GoodsStockDisBtn = new System.Windows.Forms.Button();
+            this.GoodsStockTBox = new System.Windows.Forms.TextBox();
+            this.GoodsStockLbl = new System.Windows.Forms.Label();
+            this.GoodsStockDisLbl = new System.Windows.Forms.Label();
+            this.GoodsStockUpdateBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.GoodsStockDataGridView)).BeginInit();
             this.SuspendLayout();
             // 
-            // GoodsDataGridView
+            // GoodsStockDataGridView
             // 
-            this.GoodsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.GoodsDataGridView.Location = new System.Drawing.Point(39, 24);
-            this.GoodsDataGridView.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.GoodsDataGridView.Name = "GoodsDataGridView";
-            this.GoodsDataGridView.RowTemplate.Height = 21;
-            this.GoodsDataGridView.Size = new System.Drawing.Size(927, 416);
-            this.GoodsDataGridView.TabIndex = 0;
+            this.GoodsStockDataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.GoodsStockDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.GoodsStockDataGridView.Location = new System.Drawing.Point(39, 24);
+            this.GoodsStockDataGridView.Margin = new System.Windows.Forms.Padding(4);
+            this.GoodsStockDataGridView.Name = "GoodsStockDataGridView";
+            this.GoodsStockDataGridView.ReadOnly = true;
+            this.GoodsStockDataGridView.RowTemplate.Height = 21;
+            this.GoodsStockDataGridView.Size = new System.Drawing.Size(927, 374);
+            this.GoodsStockDataGridView.TabIndex = 0;
+            // 
+            // GoodsStockDisBtn
+            // 
+            this.GoodsStockDisBtn.Location = new System.Drawing.Point(777, 455);
+            this.GoodsStockDisBtn.Name = "GoodsStockDisBtn";
+            this.GoodsStockDisBtn.Size = new System.Drawing.Size(189, 56);
+            this.GoodsStockDisBtn.TabIndex = 2;
+            this.GoodsStockDisBtn.Text = "検索";
+            this.GoodsStockDisBtn.UseVisualStyleBackColor = true;
+            this.GoodsStockDisBtn.Click += new System.EventHandler(this.GoodsStockDisBtn_Click);
+            // 
+            // GoodsStockTBox
+            // 
+            this.GoodsStockTBox.Location = new System.Drawing.Point(149, 473);
+            this.GoodsStockTBox.Name = "GoodsStockTBox";
+            this.GoodsStockTBox.Size = new System.Drawing.Size(278, 22);
+            this.GoodsStockTBox.TabIndex = 3;
+            // 
+            // GoodsStockLbl
+            // 
+            this.GoodsStockLbl.AutoSize = true;
+            this.GoodsStockLbl.Font = new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.GoodsStockLbl.Location = new System.Drawing.Point(83, 476);
+            this.GoodsStockLbl.Name = "GoodsStockLbl";
+            this.GoodsStockLbl.Size = new System.Drawing.Size(60, 15);
+            this.GoodsStockLbl.TabIndex = 4;
+            this.GoodsStockLbl.Text = "商品名：";
+            // 
+            // GoodsStockDisLbl
+            // 
+            this.GoodsStockDisLbl.AutoSize = true;
+            this.GoodsStockDisLbl.Font = new System.Drawing.Font("MS UI Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.GoodsStockDisLbl.ForeColor = System.Drawing.Color.Red;
+            this.GoodsStockDisLbl.Location = new System.Drawing.Point(83, 418);
+            this.GoodsStockDisLbl.Name = "GoodsStockDisLbl";
+            this.GoodsStockDisLbl.Size = new System.Drawing.Size(0, 17);
+            this.GoodsStockDisLbl.TabIndex = 5;
+            // 
+            // GoodsStockUpdateBtn
+            // 
+            this.GoodsStockUpdateBtn.Location = new System.Drawing.Point(563, 455);
+            this.GoodsStockUpdateBtn.Name = "GoodsStockUpdateBtn";
+            this.GoodsStockUpdateBtn.Size = new System.Drawing.Size(189, 56);
+            this.GoodsStockUpdateBtn.TabIndex = 6;
+            this.GoodsStockUpdateBtn.Text = "更新";
+            this.GoodsStockUpdateBtn.UseVisualStyleBackColor = true;
+            this.GoodsStockUpdateBtn.Click += new System.EventHandler(this.GoodsStockUpdateBtn_Click);
             // 
             // GoodsStock
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.GoodsDataGridView);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Controls.Add(this.GoodsStockUpdateBtn);
+            this.Controls.Add(this.GoodsStockDisLbl);
+            this.Controls.Add(this.GoodsStockLbl);
+            this.Controls.Add(this.GoodsStockTBox);
+            this.Controls.Add(this.GoodsStockDisBtn);
+            this.Controls.Add(this.GoodsStockDataGridView);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "GoodsStock";
             this.Size = new System.Drawing.Size(1013, 538);
             this.Load += new System.EventHandler(this.GoodsStock_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.GoodsDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GoodsStockDataGridView)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView GoodsDataGridView;
+        private System.Windows.Forms.DataGridView GoodsStockDataGridView;
+        private System.Windows.Forms.Button GoodsStockDisBtn;
+        private System.Windows.Forms.TextBox GoodsStockTBox;
+        private System.Windows.Forms.Label GoodsStockLbl;
+        private System.Windows.Forms.Label GoodsStockDisLbl;
+        private System.Windows.Forms.Button GoodsStockUpdateBtn;
     }
 }
--- Order Information Control System/OICSystem/panel/Goods/GoodsStock.cs
+++ Order Information Control System/OICSystem/panel/Goods/GoodsStock.cs
@@ -19,6 +19,9 @@ namespace WindowsFormsApplication1.panel.Goods
         OleDbDataAdapter da = new OleDbDataAdapter();
         DataTable dt = new DataTable();
 
+        string GSname;
+        string GStname;
+
         public GoodsStock()
         {
             InitializeComponent();
@@ -26,15 +29,102 @@ namespace WindowsFormsApplication1.panel.Goods
 
         private void GoodsStock_Load(object sender, EventArgs e)
         {
-            GoodsDataGridView.Columns.Clear();
-            GoodsDataGridView.DataSource = null;
+            dataload();
+        }
+
+        private void selectfunc(string cmdstr)
+        {
+            dt.Clear();
+            dt = new DataTable();
+            GoodsStockDataGridView.DataSource = null;
+            da = new OleDbDataAdapter(cmdstr, cn);
+            da.Fill(dt);
+            GoodsStockDataGridView.DataSource = dt;
+            GoodsStockDataGridView.AutoResizeColumns();
+        }
+
+        private DataTable CreateSchemaDataTable(OleDbDataReader reader)
+        {
+            if (reader == null) { return null; }
+            if (reader.IsClosed) { return null; }
+
+            DataTable schema = reader.GetSchemaTable();
+            DataTable dt = new DataTable();
+
+            foreach (DataRow row in schema.Rows)
+            {
+                // Column情報を追加してください。
+                DataColumn col = new DataColumn();
+                col.ColumnName = row["ColumnName"].ToString();
+                col.DataType = Type.GetType(row["DataType"].ToString());
+
+                if (col.DataType.Equals(typeof(string)))
+                {
+                    col.MaxLength = (int)row["ColumnSize"];
+                }
+
+                dt.Columns.Add(col);
+            }
+            return dt;
+        }
+
+        private void GoodsStockDisBtn_Click(object sender, EventArgs e)
+        {
+            cn.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;" + @"Data Source=.\DB\IM2.accdb;";
+            cn.Open();
+
+            cmd.CommandText = "SELECT * FROM 商品マスタ";
+            cmd.Connection = cn;
+
+            OleDbDataReader rd = cmd.ExecuteReader();
+
+            dt = CreateSchemaDataTable(rd);
+            DataRow row = dt.NewRow();
+
+            while (rd.Read())
+            {
+
+                GSname = (string)rd.GetValue(1);  //データベースの「商品名」
+                GStname = GoodsStockTBox.Text;    //textboxの「商品名」
+
+
+                if (GSname == GStname)
+                {
+                    selectfunc("SELECT * FROM 商品マスタ WHERE 商品名 LIKE '%" + GoodsStockTBox.Text + "%'"); //LIKEで囲んでいるが意味は無し。
+                    GoodsStockDisLbl.Text = "";
+                    break;
+                }
+
+
+
+                GoodsStockDisLbl.Text = "その情報は登録されていません";
+            }
+
+            GoodsStockTBox.Text = "";
+
+            cn.Close();
+
+        }
+
+        private void GoodsStockUpdateBtn_Click(object sender, EventArgs e)
+        {
+            dataload();
+            GoodsStockDisLbl.Text = "";
+        }
+
+        private void dataload()
+        {
+            GoodsStockDataGridView.Columns.Clear();
+            GoodsStockDataGridView.DataSource = null;
             cn.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;" + @"Data Source=.\DB\IM2.accdb;";
             da = new OleDbDataAdapter("SELECT Z.商品ID,S.商品名,Z.在庫数 FROM 在庫テーブル Z,商品マスタ S WHERE Z.商品ID=S.商品ID ORDER BY Z.商品ID ASC", cn);
             dt.Clear();
             dt = new DataTable();
             da.Fill(dt);
-            GoodsDataGridView.DataSource = dt;
-            GoodsDataGridView.AutoResizeColumns();
+            GoodsStockDataGridView.DataSource = dt;
+            GoodsStockDataGridView.AutoResizeColumns();
+
+            GoodsStockDataGridView.AllowUserToAddRows = false;      //datagridviewの最下行を消す
         }
     }
 }
