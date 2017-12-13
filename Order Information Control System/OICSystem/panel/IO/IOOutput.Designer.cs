97cedb438f8cbfce4154b24b87e8c03003e52777
higa3545 <33534879+higa3545@users.noreply.github.com>
2017-12-14 00:23:53 +0900

    出庫処理ほぼ完成
    ・IOOutout.csを編集

--- Order Information Control System/OICSystem/panel/IO/IOOutput.Designer.cs
+++ Order Information Control System/OICSystem/panel/IO/IOOutput.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             this.OutputDataGrid = new System.Windows.Forms.DataGridView();
+            this.ErrMsg = new System.Windows.Forms.Label();
+            this.OutputBtn = new System.Windows.Forms.Button();
+            this.OrderidLabel = new System.Windows.Forms.Label();
+            this.OrderidTextbox = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.OutputDataGrid)).BeginInit();
             this.SuspendLayout();
             // 
@@ -36,30 +40,76 @@
             // 
             this.OutputDataGrid.AllowUserToAddRows = false;
             this.OutputDataGrid.AllowUserToDeleteRows = false;
+            this.OutputDataGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.OutputDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.OutputDataGrid.Location = new System.Drawing.Point(3, 3);
             this.OutputDataGrid.Name = "OutputDataGrid";
             this.OutputDataGrid.ReadOnly = true;
             this.OutputDataGrid.RowTemplate.Height = 21;
-            this.OutputDataGrid.Size = new System.Drawing.Size(754, 424);
+            this.OutputDataGrid.Size = new System.Drawing.Size(754, 308);
             this.OutputDataGrid.TabIndex = 0;
+            // 
+            // ErrMsg
+            // 
+            this.ErrMsg.AutoSize = true;
+            this.ErrMsg.ForeColor = System.Drawing.Color.Red;
+            this.ErrMsg.Location = new System.Drawing.Point(91, 378);
+            this.ErrMsg.Name = "ErrMsg";
+            this.ErrMsg.Size = new System.Drawing.Size(35, 12);
+            this.ErrMsg.TabIndex = 8;
+            this.ErrMsg.Text = "label2";
+            // 
+            // OutputBtn
+            // 
+            this.OutputBtn.Location = new System.Drawing.Point(594, 341);
+            this.OutputBtn.Name = "OutputBtn";
+            this.OutputBtn.Size = new System.Drawing.Size(119, 40);
+            this.OutputBtn.TabIndex = 7;
+            this.OutputBtn.Text = "出庫完了";
+            this.OutputBtn.UseVisualStyleBackColor = true;
+            this.OutputBtn.Click += new System.EventHandler(this.OutputBtn_Click_1);
+            // 
+            // OrderidLabel
+            // 
+            this.OrderidLabel.AutoSize = true;
+            this.OrderidLabel.Location = new System.Drawing.Point(41, 341);
+            this.OrderidLabel.Name = "OrderidLabel";
+            this.OrderidLabel.Size = new System.Drawing.Size(46, 12);
+            this.OrderidLabel.TabIndex = 6;
+            this.OrderidLabel.Text = "注文ID：";
+            // 
+            // OrderidTextbox
+            // 
+            this.OrderidTextbox.Location = new System.Drawing.Point(93, 338);
+            this.OrderidTextbox.Name = "OrderidTextbox";
+            this.OrderidTextbox.Size = new System.Drawing.Size(100, 19);
+            this.OrderidTextbox.TabIndex = 5;
             // 
             // IOOutput
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
+            this.Controls.Add(this.ErrMsg);
+            this.Controls.Add(this.OutputBtn);
+            this.Controls.Add(this.OrderidLabel);
+            this.Controls.Add(this.OrderidTextbox);
             this.Controls.Add(this.OutputDataGrid);
             this.Name = "IOOutput";
             this.Size = new System.Drawing.Size(760, 430);
             this.Load += new System.EventHandler(this.IOOutput_Load);
             ((System.ComponentModel.ISupportInitialize)(this.OutputDataGrid)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView OutputDataGrid;
+        private System.Windows.Forms.Label ErrMsg;
+        private System.Windows.Forms.Button OutputBtn;
+        private System.Windows.Forms.Label OrderidLabel;
+        private System.Windows.Forms.TextBox OrderidTextbox;
     }
 }
--- Order Information Control System/OICSystem/panel/IO/IOOutput.cs
+++ Order Information Control System/OICSystem/panel/IO/IOOutput.cs
@@ -23,8 +23,35 @@ namespace WindowsFormsApplication1.panel.IO
             InitializeComponent();
         }
 
+        private DataTable CreateSchemaDataTable(OleDbDataReader reader)
+        {
+            if (reader == null) { return null; }
+            if (reader.IsClosed) { return null; }
+
+            DataTable schema = reader.GetSchemaTable();
+            DataTable dt = new DataTable();
+
+            foreach (DataRow row in schema.Rows)
+            {
+                // Column情報を追加してください。
+                DataColumn col = new DataColumn();
+                col.ColumnName = row["ColumnName"].ToString();
+                col.DataType = Type.GetType(row["DataType"].ToString());
+
+                if (col.DataType.Equals(typeof(string)))
+                {
+                    col.MaxLength = (int)row["ColumnSize"];
+                }
+
+                dt.Columns.Add(col);
+            }
+            return dt;
+        }
+
         private void IOOutput_Load(object sender, EventArgs e)
         {
+            ErrMsg.Visible = false;
+
             //クリア
             dt.Clear();
             OutputDataGrid.Columns.Clear();
@@ -32,13 +59,135 @@ namespace WindowsFormsApplication1.panel.IO
 
             dt = new DataTable();
             cn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;" + @"Data Source=.\DB\IM2.accdb;");
-
-            //データグリッドビューに注文状況を表示
-            da = new OleDbDataAdapter("select * from 発注テーブル left join 商品マスタ on (発注テーブル.商品ID =商品マスタ.商品ID) left join 入庫先マスタ on (発注テーブル.入庫先ID =入庫先マスタ.入庫先ID);", cn);
+            da = new OleDbDataAdapter("SELECT 出庫テーブル.出庫ID, 出庫テーブル.注文ID, 出庫テーブル.出庫日, 顧客テーブル.名前 AS 顧客名, 注文テーブル.[フラグ] FROM 顧客テーブル INNER JOIN(注文テーブル INNER JOIN 出庫テーブル ON 注文テーブル.注文ID = 出庫テーブル.注文ID) ON 顧客テーブル.顧客ID = 注文テーブル.顧客ID ORDER BY 出庫テーブル.出庫ID", cn);
             da.Fill(dt);
             OutputDataGrid.DataSource = dt;
-
             OutputDataGrid.AllowUserToAddRows = false;
+        }
+
+        private void OutputBtn_Click_1(object sender, EventArgs e)
+        {
+            ErrMsg.Visible = false;
+            if (OrderidTextbox.Text == null || OrderidTextbox.Text == "")
+            {
+                ErrMsg.Text = "※注文IDを入力してください";
+                ErrMsg.Visible = true;
+            }
+            else
+            {
+                DateTime dtNow = DateTime.Now;
+                int flag = 0;
+
+                dt = new DataTable();
+                cn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;" + @"Data Source=.\DB\IM2.accdb;");
+
+                cn.Open();
+                cmd.CommandText = "SELECT 注文ID,入金済み FROM 注文テーブル";
+                cmd.Connection = cn;
+                OleDbDataReader rd = cmd.ExecuteReader();
+
+                dt = CreateSchemaDataTable(rd);
+                DataRow row = dt.NewRow();
+
+                int db_Orderid = 0;
+                bool db_Pay;
+
+                while (rd.Read())
+                {
+                    db_Orderid = (int)rd.GetValue(0);
+                    db_Pay = (bool)rd.GetValue(1);
+                    if (db_Orderid == int.Parse(OrderidTextbox.Text))
+                    {
+                        flag = 1;
+                        if (db_Pay != true)
+                        {
+                            flag = 3;
+                        }
+                    }
+                }
+                cn.Close();
+
+                cn.Open();
+                cmd.CommandText = "SELECT 注文ID FROM 出庫テーブル";
+                cmd.Connection = cn;
+                OleDbDataReader rd2 = cmd.ExecuteReader();
+
+                dt = CreateSchemaDataTable(rd2);
+                DataRow row2 = dt.NewRow();
+
+                while (rd2.Read())
+                {
+                    db_Orderid = (int)rd2.GetValue(0);
+                    if (db_Orderid == int.Parse(OrderidTextbox.Text))
+                    {
+                        flag = 2;
+                    }
+                }
+                cn.Close();
+
+                if (flag == 1)
+                {
+                    OleDbCommand cmd2 = new OleDbCommand();
+                    cmd2.Connection = cn;
+                    cmd2.CommandText = "INSERT INTO 出庫テーブル (注文ID,出庫日)" + "VALUES (@orderid,@outputdate) ";
+                    OleDbParameter prorderid = new OleDbParameter("@orderid", OrderidTextbox.Text);
+                    cmd2.Parameters.Add(prorderid);
+                    OleDbParameter proutputdate = new OleDbParameter("@outputdate", dtNow.ToString("MM/dd"));
+                    cmd2.Parameters.Add(proutputdate);
+
+
+                    OleDbCommand cmd3 = new OleDbCommand();
+                    cmd3.Connection = cn;
+                    cmd3.CommandText = "UPDATE 注文テーブル SET フラグ='出庫済み' WHERE 注文ID=" + OrderidTextbox.Text + " AND 入金済み=True";
+                    try
+                    {
+                        cn.Open();
+                        cmd2.ExecuteNonQuery();
+                        cmd3.ExecuteNonQuery();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "OICSystem");
+                    }
+                    cn.Close();
+
+                    //クリア
+                    dt.Clear();
+                    OutputDataGrid.Columns.Clear();
+                    OutputDataGrid.DataSource = null;
+
+                    dt = new DataTable();
+                    cn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;" + @"Data Source=.\DB\IM2.accdb;");
+                    da = new OleDbDataAdapter("SELECT 出庫テーブル.出庫ID, 出庫テーブル.注文ID, 出庫テーブル.出庫日, 顧客テーブル.名前 AS 顧客名, 注文テーブル.[フラグ] FROM 顧客テーブル INNER JOIN(注文テーブル INNER JOIN 出庫テーブル ON 注文テーブル.注文ID = 出庫テーブル.注文ID) ON 顧客テーブル.顧客ID = 注文テーブル.顧客ID ORDER BY 出庫テーブル.出庫ID", cn);
+                    da.Fill(dt);
+                    OutputDataGrid.DataSource = dt;
+                    OutputDataGrid.AllowUserToAddRows = false;
+
+                    cmd.Parameters.Clear();
+                    cmd2.Parameters.Clear();
+                    cmd3.Parameters.Clear();
+                    OrderidTextbox.Text = "";
+                }
+                else if (flag == 0)
+                {
+                    ErrMsg.Text = "※存在しない注文IDです";
+                    ErrMsg.Visible = true;
+                    OrderidTextbox.Text = "";
+                }
+                else if (flag == 2)
+                {
+                    ErrMsg.Text = "※すでに登録されています";
+                    ErrMsg.Visible = true;
+                    OrderidTextbox.Text = "";
+                }
+                else if (flag == 3)
+                {
+                    ErrMsg.Text = "※まだ入金が完了していません";
+                    ErrMsg.Visible = true;
+                    OrderidTextbox.Text = "";
+                }
+
+            }
         }
     }
 }
