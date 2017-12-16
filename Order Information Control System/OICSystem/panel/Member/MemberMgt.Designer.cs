a4a9ec3431ebc78d6e019e3772e57d51678ea2b2
higa3545 <33534879+higa3545@users.noreply.github.com>
2017-12-16 15:50:31 +0900

    印刷機能追加

--- Order Information Control System/OICSystem/panel/Member/MemberMgt.Designer.cs
+++ Order Information Control System/OICSystem/panel/Member/MemberMgt.Designer.cs
@@ -37,24 +37,25 @@
             this.MemberClearBtn = new System.Windows.Forms.Button();
             this.MemberUpdateBtn = new System.Windows.Forms.Button();
             this.MemberEditBtn = new System.Windows.Forms.Button();
+            this.PrintBtn = new System.Windows.Forms.Button();
+            this.printDialog1 = new System.Windows.Forms.PrintDialog();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
             ((System.ComponentModel.ISupportInitialize)(this.MemberDataGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // MemberNameTBox
             // 
             this.MemberNameTBox.ImeMode = System.Windows.Forms.ImeMode.KatakanaHalf;
-            this.MemberNameTBox.Location = new System.Drawing.Point(169, 530);
-            this.MemberNameTBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MemberNameTBox.Location = new System.Drawing.Point(127, 424);
             this.MemberNameTBox.Name = "MemberNameTBox";
-            this.MemberNameTBox.Size = new System.Drawing.Size(197, 22);
+            this.MemberNameTBox.Size = new System.Drawing.Size(149, 19);
             this.MemberNameTBox.TabIndex = 1;
             // 
             // MemberDisBtn
             // 
-            this.MemberDisBtn.Location = new System.Drawing.Point(581, 465);
-            this.MemberDisBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MemberDisBtn.Location = new System.Drawing.Point(436, 372);
             this.MemberDisBtn.Name = "MemberDisBtn";
-            this.MemberDisBtn.Size = new System.Drawing.Size(201, 46);
+            this.MemberDisBtn.Size = new System.Drawing.Size(151, 37);
             this.MemberDisBtn.TabIndex = 2;
             this.MemberDisBtn.Text = "検索";
             this.MemberDisBtn.UseVisualStyleBackColor = true;
@@ -63,20 +64,18 @@
             // MemberNameLbl
             // 
             this.MemberNameLbl.AutoSize = true;
-            this.MemberNameLbl.Location = new System.Drawing.Point(108, 533);
-            this.MemberNameLbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.MemberNameLbl.Location = new System.Drawing.Point(81, 426);
             this.MemberNameLbl.Name = "MemberNameLbl";
-            this.MemberNameLbl.Size = new System.Drawing.Size(53, 15);
+            this.MemberNameLbl.Size = new System.Drawing.Size(42, 12);
             this.MemberNameLbl.TabIndex = 51;
             this.MemberNameLbl.Text = "ﾌﾘｶﾞﾅ：";
             // 
             // MemberMgrLbl
             // 
             this.MemberMgrLbl.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.MemberMgrLbl.Location = new System.Drawing.Point(25, 21);
-            this.MemberMgrLbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.MemberMgrLbl.Location = new System.Drawing.Point(19, 17);
             this.MemberMgrLbl.Name = "MemberMgrLbl";
-            this.MemberMgrLbl.Size = new System.Drawing.Size(128, 45);
+            this.MemberMgrLbl.Size = new System.Drawing.Size(96, 36);
             this.MemberMgrLbl.TabIndex = 50;
             this.MemberMgrLbl.Text = "顧客管理";
             this.MemberMgrLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -85,12 +84,11 @@
             // 
             this.MemberDataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.MemberDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.MemberDataGridView.Location = new System.Drawing.Point(28, 81);
-            this.MemberDataGridView.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MemberDataGridView.Location = new System.Drawing.Point(21, 65);
             this.MemberDataGridView.Name = "MemberDataGridView";
             this.MemberDataGridView.ReadOnly = true;
             this.MemberDataGridView.RowTemplate.Height = 21;
-            this.MemberDataGridView.Size = new System.Drawing.Size(964, 358);
+            this.MemberDataGridView.Size = new System.Drawing.Size(723, 286);
             this.MemberDataGridView.TabIndex = 6;
             // 
             // MemberDisLbl
@@ -98,18 +96,16 @@
             this.MemberDisLbl.AutoSize = true;
             this.MemberDisLbl.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.MemberDisLbl.ForeColor = System.Drawing.Color.Red;
-            this.MemberDisLbl.Location = new System.Drawing.Point(56, 475);
-            this.MemberDisLbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.MemberDisLbl.Location = new System.Drawing.Point(42, 380);
             this.MemberDisLbl.Name = "MemberDisLbl";
-            this.MemberDisLbl.Size = new System.Drawing.Size(0, 20);
+            this.MemberDisLbl.Size = new System.Drawing.Size(0, 16);
             this.MemberDisLbl.TabIndex = 63;
             // 
             // MemberClearBtn
             // 
-            this.MemberClearBtn.Location = new System.Drawing.Point(581, 519);
-            this.MemberClearBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MemberClearBtn.Location = new System.Drawing.Point(436, 415);
             this.MemberClearBtn.Name = "MemberClearBtn";
-            this.MemberClearBtn.Size = new System.Drawing.Size(201, 46);
+            this.MemberClearBtn.Size = new System.Drawing.Size(151, 37);
             this.MemberClearBtn.TabIndex = 3;
             this.MemberClearBtn.Text = "クリア";
             this.MemberClearBtn.UseVisualStyleBackColor = true;
@@ -117,10 +113,9 @@
             // 
             // MemberUpdateBtn
             // 
-            this.MemberUpdateBtn.Location = new System.Drawing.Point(791, 519);
-            this.MemberUpdateBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MemberUpdateBtn.Location = new System.Drawing.Point(593, 415);
             this.MemberUpdateBtn.Name = "MemberUpdateBtn";
-            this.MemberUpdateBtn.Size = new System.Drawing.Size(201, 46);
+            this.MemberUpdateBtn.Size = new System.Drawing.Size(151, 37);
             this.MemberUpdateBtn.TabIndex = 5;
             this.MemberUpdateBtn.Text = "更新";
             this.MemberUpdateBtn.UseVisualStyleBackColor = true;
@@ -128,19 +123,38 @@
             // 
             // MemberEditBtn
             // 
-            this.MemberEditBtn.Location = new System.Drawing.Point(791, 465);
-            this.MemberEditBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MemberEditBtn.Location = new System.Drawing.Point(593, 372);
             this.MemberEditBtn.Name = "MemberEditBtn";
-            this.MemberEditBtn.Size = new System.Drawing.Size(201, 46);
+            this.MemberEditBtn.Size = new System.Drawing.Size(151, 37);
             this.MemberEditBtn.TabIndex = 4;
             this.MemberEditBtn.Text = "編集";
             this.MemberEditBtn.UseVisualStyleBackColor = true;
             this.MemberEditBtn.Click += new System.EventHandler(this.MemberEditBtn_Click);
             // 
+            // PrintBtn
+            // 
+            this.PrintBtn.Location = new System.Drawing.Point(622, 17);
+            this.PrintBtn.Name = "PrintBtn";
+            this.PrintBtn.Size = new System.Drawing.Size(75, 23);
+            this.PrintBtn.TabIndex = 64;
+            this.PrintBtn.Text = "印刷";
+            this.PrintBtn.UseVisualStyleBackColor = true;
+            this.PrintBtn.Click += new System.EventHandler(this.PrintBtn_Click);
+            // 
+            // printDialog1
+            // 
+            this.printDialog1.UseEXDialog = true;
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.EndPrint += new System.Drawing.Printing.PrintEventHandler(this.printDocument1_EndPrint);
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
             // MemberMgt
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.PrintBtn);
             this.Controls.Add(this.MemberEditBtn);
             this.Controls.Add(this.MemberUpdateBtn);
             this.Controls.Add(this.MemberClearBtn);
@@ -150,9 +164,8 @@
             this.Controls.Add(this.MemberDisBtn);
             this.Controls.Add(this.MemberNameLbl);
             this.Controls.Add(this.MemberMgrLbl);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "MemberMgt";
-            this.Size = new System.Drawing.Size(1013, 611);
+            this.Size = new System.Drawing.Size(760, 489);
             this.Load += new System.EventHandler(this.MemberMgt_Load);
             ((System.ComponentModel.ISupportInitialize)(this.MemberDataGridView)).EndInit();
             this.ResumeLayout(false);
@@ -171,5 +184,8 @@
         private System.Windows.Forms.Button MemberClearBtn;
         private System.Windows.Forms.Button MemberUpdateBtn;
         private System.Windows.Forms.Button MemberEditBtn;
+        private System.Windows.Forms.Button PrintBtn;
+        private System.Windows.Forms.PrintDialog printDialog1;
+        private System.Drawing.Printing.PrintDocument printDocument1;
     }
 }
